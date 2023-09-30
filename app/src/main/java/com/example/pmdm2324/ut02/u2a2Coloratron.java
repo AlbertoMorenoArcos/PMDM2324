d6e3d025e937ae7c5e15e5a20df2f42e501fcc08
Alberto <albertomorenoarcos@gmail.com>
2023-09-30 16:41:46 +0200

    coloratron acabado

--- app/src/main/java/com/example/pmdm2324/ut02/u2a2Coloratron.java
+++ app/src/main/java/com/example/pmdm2324/ut02/u2a2Coloratron.java
@@ -2,9 +2,11 @@ package com.example.pmdm2324.ut02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -15,10 +17,9 @@ import com.example.pmdm2324.R;
 public class u2a2Coloratron extends AppCompatActivity {
 
     EditText etEntrada;
-    TextView tvSalida;
-    TextView cambioColorTexto;
+    TextView tvSalida, tvColores, tvBlanco;
     Button btGenerar;
-    SeekBar sbColorRojo;
+    SeekBar sbColorRojo, sbColorVerde, sbColorAzul;
     ToggleButton tbColorTexto;
 
 
@@ -31,20 +32,38 @@ public class u2a2Coloratron extends AppCompatActivity {
 
         etEntrada = findViewById(R.id.u2a2idetIntroducir);
         tvSalida = findViewById(R.id.u2a2idtvMostrarTexto);
-        cambioColorTexto = findViewById(R.id.u2a2idtvTextoBlancoNegro);
+        tvBlanco = findViewById(R.id.u2a2idtvBlanco);
+        tvColores = findViewById(R.id.u2a2idtvColores);
         btGenerar = findViewById(R.id.u2a2idbtGenerar);
         sbColorRojo = findViewById(R.id.u2a2idsbColorRojo);
+        sbColorVerde = findViewById(R.id.u2a2idsbColorVerde);
+        sbColorAzul = findViewById(R.id.u2a2idsbColorAzul);
         tbColorTexto = findViewById(R.id.u2a2idtbColorTexto);
 
+
+
+        sbColorRojo.setBackgroundColor(Color.RED);
+        sbColorVerde.setBackgroundColor(Color.GREEN);
+        sbColorAzul.setBackgroundColor(Color.BLUE);
+
+        tbColorTexto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    tvSalida.setTextColor(Color.WHITE);
+                } else {
+                    tvSalida.setTextColor(Color.BLACK);
+                }
+            }
+        });
         btGenerar.setOnClickListener((View v)->{
+            int rojo = sbColorRojo.getProgress();
+            int verde = sbColorVerde.getProgress();
+            int azul = sbColorAzul.getProgress();
+
             String texto = etEntrada.getText().toString();
             tvSalida.setText(texto);
+            tvSalida.setBackgroundColor(Color.rgb(rojo,verde,azul));
 
-            if(tbColorTexto.isChecked()){
-                tvSalida.setTextColor(000000);
-            }else{
-                tvSalida.setTextColor(FFFFFF);
-            }
 
         });
     }
