226edcd0f72bb84082c01859678f5cc6a15c21b6
alberto moreno arcos <albertomorenoarcos@gmail.com>
2023-10-03 20:47:16 +0200

    Mi primer app

--- app/src/main/java/com/example/pmdm2324/ut02/Propinatron3000.java
+++ app/src/main/java/com/example/pmdm2324/ut02/Propinatron3000.java
@@ -3,6 +3,7 @@ package com.example.pmdm2324.ut02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -13,9 +14,9 @@ public class Propinatron3000 extends AppCompatActivity {
 
     Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt0, btBorrarUno, btBorrarTodo, btGenerarPropina;
     TextView tvCuenta, tvCuentaPropina;
-
     RadioButton rbMala, rbBuena, rbExcelente;
 
+    View.OnClickListener manejador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +41,19 @@ public class Propinatron3000 extends AppCompatActivity {
         rbBuena = findViewById(R.id.u2a2idrbBuena);
         rbExcelente = findViewById(R.id.u2a2idrbExcelente);
 
-
-
+        manejador = (View BotonPulsado)->{
+            Button btPulsado = (Button) BotonPulsado;
+            tvCuenta.setText(btPulsado.getText());
+        };
+        bt1.setOnClickListener(manejador);
+        bt2.setOnClickListener(manejador);
+        bt3.setOnClickListener(manejador);
+        bt4.setOnClickListener(manejador);
+        bt5.setOnClickListener(manejador);
+        bt6.setOnClickListener(manejador);
+        bt7.setOnClickListener(manejador);
+        bt8.setOnClickListener(manejador);
+        bt9.setOnClickListener(manejador);
+        bt0.setOnClickListener(manejador);
     }
 }
