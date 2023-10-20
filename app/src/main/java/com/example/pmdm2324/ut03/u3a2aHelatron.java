d986c8d553735cc514bef7a2e74db38d21754073
alberto moreno arcos <albertomorenoarcos@gmail.com>
2023-10-20 17:54:42 +0200

    helatron

--- app/src/main/java/com/example/pmdm2324/ut03/u3a2aHelatron.java
+++ app/src/main/java/com/example/pmdm2324/ut03/u3a2aHelatron.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,37 +15,81 @@ import android.widget.TextView;
 
 import com.example.pmdm2324.R;
 
-public class u3a2aHelatron extends AppCompatActivity {
+public class u3a2aHelatron extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     EditText etVainilla, etChocolate, etFresa;
     Spinner spRecipiente;
     Button btElegir;
+    TextView tvError;
     public static final String INFO_CHOCO = "chocolate";
     public static final String INFO_FRESA = "fresa";
     public static final String INFO_VAINILLA = "vainilla";
-    public static final String INFO_SPINNER = "spinner";
+    public static final String INFO_RECIPIENTE = "recipiente";
+    String recipiente;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u3a2a_helatron);
 
-        etChocolate = findViewById(R.id.u3a2idetBolasVaini);
+        etChocolate = findViewById(R.id.u3a2idetBolasChoco);
         etFresa = findViewById(R.id.u3a2idetBolasFresa);
         etVainilla = findViewById(R.id.u3a2idetBolasVaini);
         spRecipiente = findViewById(R.id.u3a2idspRecipiente);
         btElegir = findViewById(R.id.u3a2idbtElegir);
-        Intent i = new Intent(this, u3a1bResultado.class);
+        tvError = findViewById(R.id.u3a2idtvError);
+
 
         btElegir.setOnClickListener(view -> {
+            Intent i = new Intent(this, u3a2bHelatron.class);
+            String bolasFresa = etFresa.getText().toString();
+            String bolasChoco = etChocolate.getText().toString();
+            String bolasVaini = etVainilla.getText().toString();
 
-
+            if(!bolasFresa.isEmpty() && !bolasChoco.isEmpty() && !bolasVaini.isEmpty() && !recipiente.isEmpty()){
+                i.putExtra(INFO_FRESA, bolasFresa);
+                i.putExtra(INFO_CHOCO, bolasChoco);
+                i.putExtra(INFO_VAINILLA, bolasVaini);
+                i.putExtra(INFO_RECIPIENTE, recipiente);
 
                 startActivity(i);
+            }else{
+                tvError.setText("Rellena todos los datos.");
             }
+
+
+
         });
 
+        spRecipiente.setOnItemSelectedListener(this);
+    String[] recipientes = { "Cucurucho", "Cucurucho Chocolate", "Tarrina" };
+    // Create the instance of ArrayAdapter
+    // having the list of courses
+    ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, recipientes);
 
+    // set simple layout resource file
+    // for each item of spinner
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+    // Set the ArrayAdapter (ad) data on the
+    // Spinner which binds data to spinner
+        spRecipiente.setAdapter(ad);
+
+}
+    // Performing action when ItemSelected
+    // from spinner, Overriding onItemSelected method
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        recipiente = spRecipiente.getItemAtPosition(i).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
+
+
+
--- app/src/main/java/com/example/pmdm2324/ut03/u3a2bHelatron.java
+++ app/src/main/java/com/example/pmdm2324/ut03/u3a2bHelatron.java
@@ -2,15 +2,55 @@ package com.example.pmdm2324.ut03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.pmdm2324.R;
 
 public class u3a2bHelatron extends AppCompatActivity {
 
+    TextView tvFresa, tvChoco, tvVainilla, tvRecipiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u3a2b_helatron);
+
+        tvFresa = findViewById(R.id.u3a2bidtvFresa);
+        tvChoco = findViewById(R.id.u3a2bidtvChocolate);
+        tvVainilla = findViewById(R.id.u3a2bidtvVainilla);
+        tvRecipiente = findViewById(R.id.u3a2bidtvRecipiente);
+
+        Bundle info = getIntent().getExtras();
+
+        String bolasFresa = info.getString(u3a2aHelatron.INFO_FRESA);
+        int bFresa = Integer.parseInt(bolasFresa);
+        String  bolasChoco = info.getString(u3a2aHelatron.INFO_CHOCO);
+        int bChoco = Integer.parseInt(bolasChoco);
+        String bolasVainilla = info.getString(u3a2aHelatron.INFO_VAINILLA);
+        int bVainilla = Integer.parseInt(bolasVainilla);
+        String recipiente = info.getString(u3a2aHelatron.INFO_RECIPIENTE);
+
+        for(int i = 1; i<=bFresa; i++){
+            String o = "O";
+            tvFresa.append(o);
+        }
+        for(int i = 1; i<=bChoco; i++){
+            String o = "O";
+            tvChoco.append(o);
+        }
+        for(int i = 1; i<=bVainilla; i++){
+            String o = "O";
+            tvVainilla.append(o);
+        }
+        if(recipiente.equals("Cucurucho")){
+            tvRecipiente.setText("V");
+        } else if (recipiente.equals("Cucurucho Chocolate")) {
+         tvRecipiente.setText("V");
+         tvRecipiente.setTextColor(Color.rgb(128,64,0));
+        } else if (recipiente.equals("Tarrina")) {
+            tvRecipiente.setText("U");
+        }
+
     }
 }
