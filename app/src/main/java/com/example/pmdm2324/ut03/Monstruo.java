98e6d3a3467a3c0b4c9ac09d8c4d00e7240e09bf
alberto moreno arcos <albertomorenoarcos@gmail.com>
2023-11-14 17:32:53 +0100

    monstruo falta por acabar

--- app/src/main/java/com/example/pmdm2324/ut03/Monstruo.java
+++ app/src/main/java/com/example/pmdm2324/ut03/Monstruo.java
@@ -6,45 +6,44 @@ import java.util.Random;
 public class Monstruo implements Serializable {
     public static final String CLAVE_MONSTRUO = "monstruo";
     private String nombre;
-    private int manos;
-    private int piernas;
+    /*private int manos;
+    private int piernas;*/
+    private int extremidades;
     private String color;
 
-    public Monstruo(String nombre, String color) {
+    public Monstruo(String nombre,int extremidades, String color) {
         this.nombre = nombre;
+        this.extremidades = extremidades;
         this.color = color;
-        this.manos = generarNumeroAleatorio(1, 10); // Número aleatorio entre 1 y 4 para las manos
-        this.piernas = generarNumeroAleatorio(1, 10); // Número aleatorio entre 1 y 4 para las piernas
+       // this.manos = generarNumeroAleatorio(1, 10); // Número aleatorio entre 1 y 4 para las manos
+       // this.piernas = generarNumeroAleatorio(1, 10); // Número aleatorio entre 1 y 4 para las piernas
     }
     public String getColor() {
         return color;
     }
 
-    private int generarNumeroAleatorio(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min + 1) + min;
-    }
+
 
     @Override
     public String toString() {
         StringBuilder monstruo = new StringBuilder();
 
-        monstruo.append("Nombre: ").append(nombre).append("\n");
+        /*monstruo.append("Nombre: ").append(nombre).append("\n");
         monstruo.append("Color: ").append(color).append("\n");
-        monstruo.append("Manos: ").append(manos).append("\n");
-        monstruo.append("Piernas: ").append(piernas).append("\n");
+        monstruo.append("Manos: ").append(extremidades).append("\n");
+        monstruo.append("Piernas: ").append(extremidades).append("\n");*/
 
         monstruo.append("   *\n");
 
-        for (int i = 0; i < piernas; i++) {
+        for (int i = 0; i < extremidades; i++) {
             monstruo.append("// ");
         }
         monstruo.append("\n");
 
-        for (int i = 0; i < piernas; i++) {
+        for (int i = 0; i < extremidades; i++) {
             monstruo.append("///");
         }
-        monstruo.append(" \\\\\n");
+        monstruo.append(" \\");
 
         return monstruo.toString();
     }
